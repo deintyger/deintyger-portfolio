66d92cd8bf6c5f3e01dc37383add32bb412bf2e3
Dein Tyger <you@example.com>
2025-10-26 13:50:42 +0100

    chore: add index.ts and assets.d.ts

---
+++ dein-portfolio/src/types/assets.d.ts
@@ -0,0 +1,34 @@
+// Specific declaration for the generated assets index
+declare module "../assets/assets" {
+  export const logo: string
+  export const backend: string
+  export const creator: string
+  export const mobile: string
+  export const web: string
+  export const github: string
+  export const menu: string
+  export const close: string
+
+  export const css: string
+  export const docker: string
+  export const figma: string
+  export const git: string
+  export const html: string
+  export const javascript: string
+  export const mongodb: string
+  export const nodejs: string
+  export const reactjs: string
+  export const redux: string
+  export const tailwind: string
+  export const typescript: string
+  export const threejs: string
+
+  export const meta: string
+  export const shopify: string
+  export const starbucks: string
+  export const tesla: string
+
+  export const carrent: string
+  export const jobit: string
+  export const tripguide: string
+}
