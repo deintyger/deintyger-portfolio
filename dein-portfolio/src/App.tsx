74601f238c0fc8ebe62580079e854e45e3b6a501
Dein Tyger <you@example.com>
2025-10-26 11:38:54 +0100

    init: set up base project with Tailwind and style configuration

---
+++ dein-portfolio/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <div>Dein Tyger Portfolio</div>;
+};
+
+export default App;
---
+++ dein-portfolio/src/mesage.tsx
@@ -0,0 +1,6 @@
+function Message() {
+    const name = "dein";
+    return <h1>hello {name}</h1>;
+}
+
+export default Message;
